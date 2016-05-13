afc1898b6f347538bf51e4e26792d34cf3e38634
wdlyons <wdlyons@lyons-barton.com>
2016-05-13 12:41:20 +1000

    Initial File
    Blank - No coding

---
+++ install.php
@@ -0,0 +1,23 @@
+<?php ob_start();
+
+/*
+
+Plugin Name:  WDL Family History and Genealogy Pedigree Chart
+
+Plugin URI: http://www.warwicklyons.com.au/wordpress-family-history-plugin/
+
+Description: Add Pedigree charts, Sibling, Spouse and Children lists to your website 
+
+Version: 1.0.6
+
+Author: Warwick Lyons
+
+Author URI: http://warwicklyons.com.au
+
+License: © Copyright 2014. All Rights Reserved
+
+*/
+
+
+
+?>
